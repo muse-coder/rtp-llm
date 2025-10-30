e00daca361b78a6376c52f75fb5906d091d7bf7c
lxq <lxq271332@alibaba-inc.com>
2025-10-30 14:16:58 +0800

    feat: add memory connector

--- rtp_llm/cpp/cache_new/KVCacheManager.h
+++ rtp_llm/cpp/cache_new/KVCacheManager.h
@@ -13,25 +13,25 @@
 
 class KVCacheManager {
 public:
-    bool init();
+    bool               init();
     const CacheConfig& cacheConfig() const;
     // size_t availableBlockNums() const;
     // size_t freeBlockNums() const;
 
     size_t availableTokenNums() const;
-    
+
     CacheLayerLayout layerCacheBase() const;
 
-    KVCacheManager(const CacheConfig&      config,
-        rtp_llm::DeviceBase*               device,
-        bool                               warmup           = false,
-        const kmonitor::MetricsReporterPtr metrics_reporter = nullptr,
-        const GptInitParameter&            params           = GptInitParameter{});
+    KVCacheManager(const CacheConfig&                 config,
+                   rtp_llm::DeviceBase*               device,
+                   bool                               warmup           = false,
+                   const kmonitor::MetricsReporterPtr metrics_reporter = nullptr,
+                   const GptInitParameter&            params           = GptInitParameter{});
     ~KVCacheManager();
 
     MallocResult malloc(const MallocInfo& malloc_info);
-    FreeResult free(const FreeInfo& free_info);
-    InsertResult insertIntoCache(const InsertInfo& insert_info); 
+    FreeResult   free(const FreeInfo& free_info);
+    InsertResult insertIntoCache(const InsertInfo& insert_info);
 
     // connector need kvCacheGroups to transfer caches
     std::vector<std::shared_ptr<KVCacheGroup>> kvCacheGroups() const {
@@ -40,6 +40,10 @@ public:
 
     // groupInfo groupInfo()
     // TODO: InsertInfo 考虑  hicache distkvcache 的逻辑
+
+private:
+    InsertResult insertIntoMemoryCache(const InsertInfo& insert_info);
+
 private:
     CacheConfig config_;
     rtp_llm::DeviceBase* device_;
@@ -49,8 +53,7 @@ private:
     const GptInitParameter& params_;
 
     HashUtil hash_util_;
-    std::shared_ptr<MemoryBlockCache> memory_block_cache_;
-    std::shared_ptr<KVCacheReaderWriter> kv_cache_reader_writer_;
-    std::shared_ptr<Notifier> notifier_;
-    // HashUtil hash_util_;
+
+    std::shared_ptr<KVCacheReaderWriter> mem_reader_writer_;
+    std::shared_ptr<KVCacheReaderWriter> remote_reader_writer_;
 };
